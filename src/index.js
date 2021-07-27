32ab4b0153efd9e317c54a0931c83ac2e04d3256
mason roberts <mrobe18@gmail.com>
2021-07-27 14:40:57 -0400

    removed some unwanted files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 
@@ -15,10 +14,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-//* I use whole BrowserRouter for two reasons. One, I only type it once and two, Router is close to Route and who needs the confusion.
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
